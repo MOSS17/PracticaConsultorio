25e56b026101d0be6d59895f9de55370535f1ce6
Miguel Sandoval <miguel_0799@hotmail.com>
2018-11-01 08:05:41 -0700

    .

--- PracticaConsultorio/Datos.cs
+++ PracticaConsultorio/Datos.cs
@@ -9,5 +9,8 @@ namespace PracticaConsultorio
     class Datos
     {
         public static List<Paciente> pacientes = new List<Paciente>();
+
+        public static List<Consulta> consultas =
+            new List<Consulta>();
     }
 }
--- PracticaConsultorio/MainWindow.xaml.cs
+++ PracticaConsultorio/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace PracticaConsultorio
     /// </summary>
     public partial class MainWindow : Window
     {
+        DateTime fechaInicioConsulta;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,43 +29,140 @@ namespace PracticaConsultorio
 
         private void btnGuardarNuevoPaciente_Click(object sender, RoutedEventArgs e)
         {
-            Paciente nuevoPaciente = new Paciente();
-            nuevoPaciente.Nombre = txtNombre.Text;
-            nuevoPaciente.Direccion = txtDireccion.Text;
-            nuevoPaciente.Telefono = txtTelefono.Text;
-            nuevoPaciente.Edad = int.Parse(txtEdad.Text);
-            nuevoPaciente.Peso = float.Parse(txtPeso.Text);
-            nuevoPaciente.Altura = float.Parse(txtAltura.Text);
-            nuevoPaciente.EnfermedadesCronicas = txtEnfermedadesCronicas.Text;
+            Paciente nuevoPaciente =
+                new Paciente();
+            nuevoPaciente.Nombre =
+                txtNombre.Text;
+            nuevoPaciente.Direccion =
+                txtDireccion.Text;
+            nuevoPaciente.Telefono =
+                txtTelefono.Text;
+            nuevoPaciente.Edad =
+                int.Parse(txtEdad.Text);
+            nuevoPaciente.Altura =
+                float.Parse(txtAltura.Text);
+            nuevoPaciente.Peso =
+                float.Parse(txtPeso.Text);
+            nuevoPaciente.EnfermedadesCronicas =
+                txtEnfermedadesCronicas.Text;
 
             Datos.pacientes.Add(nuevoPaciente);
             actualizarListaPacientes();
 
-            txtNombre.Text = string.Empty;
+            txtNombre.Text = "";
             txtDireccion.Text = "";
             txtTelefono.Text = "";
             txtEdad.Text = "";
             txtPeso.Text = "";
             txtAltura.Text = "";
             txtEnfermedadesCronicas.Text = "";
+
+            gridNuevoPaciente.Visibility =
+                Visibility.Collapsed;
         }
 
         private void actualizarListaPacientes()
         {
             lstPacientes.Items.Clear();
-            foreach(Paciente paciente in Datos.pacientes)
+            foreach (Paciente paciente
+                in Datos.pacientes)
             {
                 lstPacientes.Items.Add(
-                    new ListBoxItem() {
+                    new ListBoxItem()
+                    {
                         Content = paciente.Nombre
                     }
-                );
+                    );
             }
         }
 
         private void btnCrearNuevoPaciente_Click(object sender, RoutedEventArgs e)
         {
-            GridNuevoPaciente.Visibility = Visibility.Visible;
+            gridNuevoPaciente.Visibility =
+                Visibility.Visible;
+        }
+
+        private void lstPacientes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lstPacientes.SelectedIndex != -1)
+            {
+                btnNuevaConsulta.IsEnabled = true;
+            }
+            else
+            {
+                btnNuevaConsulta.IsEnabled = false;
+            }
+        }
+
+        private void btnNuevaConsulta_Click(object sender, RoutedEventArgs e)
+        {
+            gridFormularioConsulta.Visibility =
+                Visibility.Visible;
+
+            Paciente paciente =
+                Datos.pacientes[lstPacientes.SelectedIndex];
+
+            txtNombrePacienteConsulta.Text =
+                paciente.Nombre;
+            txtEdadPacienteConsulta.Text =
+                paciente.Edad.ToString();
+            txtAlturaPacienteConsulta.Text =
+                paciente.Altura.ToString();
+            txtPesoPacienteConsulta.Text =
+                paciente.Peso.ToString();
+            txtEnfermedadesPacienteConsulta.Text =
+                paciente.EnfermedadesCronicas;
+
+            fechaInicioConsulta = DateTime.Now;
+
+            txtFechaConsulta.Text =
+                fechaInicioConsulta.ToString();
+
+        }
+
+        private void btnGuardarConsulta_Click(object sender, RoutedEventArgs e)
+        {
+            Consulta nuevaConsulta =
+                new Consulta();
+            nuevaConsulta.PacienteActual =
+                Datos.pacientes[lstPacientes.SelectedIndex];
+            nuevaConsulta.Sintomas =
+                txtSintomasConsulta.Text;
+            nuevaConsulta.Diagnostico =
+                txtDiagnosticoConsulta.Text;
+            nuevaConsulta.Receta =
+                txtRecetaConsulta.Text;
+            nuevaConsulta.fecha =
+                fechaInicioConsulta;
+
+            Datos.consultas.Add(nuevaConsulta);
+
+            txtNombrePacienteConsulta.Text = "";
+            txtEdadPacienteConsulta.Text = "";
+            txtAlturaPacienteConsulta.Text = "";
+            txtPesoPacienteConsulta.Text = "";
+            txtEnfermedadesPacienteConsulta.Text = "";
+
+            txtSintomasConsulta.Text = "";
+            txtDiagnosticoConsulta.Text = "";
+            txtRecetaConsulta.Text = "";
+            txtFechaConsulta.Text = "";
+
+            var consultas = Datos.consultas;
+
+            gridFormularioConsulta.Visibility =
+                Visibility.Collapsed;
+        }
+
+        private void btnHistorialConsultas_Click(object sender, RoutedEventArgs e)
+        {
+            var paciente =
+                Datos.pacientes[lstPacientes.SelectedIndex];
+
+
+            var ventanaHistorial =
+                new HistorialConsultasWindow(paciente);
+            ventanaHistorial.Show();
         }
     }
 }
